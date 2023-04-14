2e40f6c649db34501c86af2f5f8b850f19b23b7a
John <39836597+oxwilder@users.noreply.github.com>
2023-04-14 09:33:40 -0400

    move src

--- coalesce.py
+++
@@ -1,8 +0,0 @@
-def coalesce(*args):
-    for arg in args:
-        match bool(arg):
-            case False:
-                continue
-            case _:
-                return arg
-    return None
